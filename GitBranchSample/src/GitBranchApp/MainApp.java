5ee739a6a4144c81bc778546476d2c1c4b1aeb78
LAPTOP-OK9FJSJC\CJ0201010 <tom70215@yahoo.com.tw>
2021-02-18 14:49:19 +0800

    1st Version

---
+++ GitBranchSample/src/GitBranchApp/MainApp.java
@@ -0,0 +1,11 @@
+package GitBranchApp;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        // 這是第一個版本
+		System.out.println("1st Version");
+	}
+
+}
